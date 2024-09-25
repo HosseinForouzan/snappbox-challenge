684a63684d2196529908ed1a8f19da4ea1c0760f
unknown <hosseinf97@gmail.com>
2024-09-25 15:09:39 +0330

    implement haversine function

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 import (
 	"time"
 	"sync"
+	"math"
 )
 
 type Point struct {
@@ -25,3 +26,18 @@ type FareCalculator struct {
 	waitGroup sync.WaitGroup
 }
 
+func haversine(lat1, lon1, lat2, lon2 float64) float64 {
+	const R = 6371 //Radius of the Earth
+	dLat := (lat2 - lat1) * math.Pi / 180
+	dLon := (lon2 - lon1) * math.Pi / 180
+
+	lat1 = lat1 * math.Pi / 180
+	lat2 = lat2 * math.Pi / 180
+
+	a := (math.Sin(dLat / 2) * math.Sin(dLat / 2)) + 
+		math.Sin(dLon/2)*math.Sin(dLon/2)*math.Cos(lat1)*math.Cos(lat2)
+	c := 2 * math.Atan2(math.Sqrt(a), math.Sqrt(1-a))
+	d := R * c
+
+	return d
+} 
